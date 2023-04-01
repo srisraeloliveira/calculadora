5dc843ff4fbc343ce9619593367930fc042a893d
srisraeloliveira <isra.dias.oliveira@gmail.com>
2023-04-01 20:44:36 -0300

    refactor: functions constructors

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,82 +1,61 @@
-function criaCalc() {
-  return {
-    display: document.querySelector(".display"),
-    //btnClear: document.querySelector(".btn-clear"),
-    
-    //ctrl + F2 -> mudar o nome de uma variavel em varios lugares ao mesmo tempo
-    init() {
-      this.cliqueBtn(); //para executar as funções na tela
-      this.keyEnter();  //para executar as funções no teclado
-    },
+//Funçao Construtora
 
-    //Para limpar o display da calculadora
-    clearDisplay() {
-      this.display.value = "";
-    },
-
-    //Para apagar o último digito digitado
-    delOne() {
-      this.display.value = this.display.value.slice(0, -1);
-    },
-    //Adiciona o evento de enter para executar a conta;     
-    keyEnter() {
-      this.display.addEventListener("keyup", (e) => { 
-        if (e.keyCode === 13) {
-          this.equals();
-        }
-      });
-    },
-
-    //Para resolver a conta, foi utilizado o metodo eval, porem não é um método seguro.
-    equals() {
-      let conta = this.display.value;
-      try {
-        conta = eval(conta);
-        if (!conta) {
-          alert("Conta inválida");
-          return;
-        }
-        this.display.value = String(conta);
-      } catch (e) {
+//instanciando como função construtora
+function Calc() {
+  this.display = document.querySelector(".display");
+  //Funções dependendentes de Calc para iniciar o programa
+  this.inicia = () => {
+    this.captureClick();
+    this.keyEnter();
+  };
+  //Função para permitir o resultado com a tecla enter
+  this.keyEnter = () => {
+    this.display.addEventListener("keydown", (e) => {
+      if (e.keyCode === 13) this.equalsDisplay();
+    });
+  };
+  //Por se tratar de uma função construtora, podemos capturar todos os eventos conjuntamente
+  this.captureClick = () => {
+    document.addEventListener("click", (e) => {
+      const el = e.target;
+      if (el.classList.contains("btn-num")) this.addNumDisplay(el);
+      if (el.classList.contains("btn-clear")) this.clearDisplay();
+      if (el.classList.contains("btn-del")) this.delDisplay();
+      if (el.classList.contains("btn-eq")) this.equalsDisplay();
+    });
+  };
+  //adicionando numero no display
+  this.addNumDisplay = (el) => {
+    this.display.value += el.innerText;
+  };
+  //limpando o display
+  this.clearDisplay = () => {
+    this.display.value = "";
+  };
+  //apagando ultimo digito do display
+  this.delDisplay = () => {
+    this.display.value = this.display.value.slice(0, -1);
+  };
+  //executando calculo, utilizando eval
+  this.equalsDisplay = () => {
+    let conta = this.display.value;
+    try {
+      conta = eval(conta);
+      if (!conta) {
         alert("Conta inválida");
         return;
       }
-    },
-    
-    //Função para capturar o clique no display
-    cliqueBtn() {
-      document.addEventListener("click", (e) => {
-        const el = e.target;
-
-        //capturando numeros e operadores
-        if (el.classList.contains("btn-num")) {
-          this.btnParaDisplay(el.innerText);
-        }
-
-        //capturando o clear
-        if (el.classList.contains("btn-clear")) {
-          this.clearDisplay();
-        }
-
-        //capturando o delete -> ultimo numero
-        if (el.classList.contains("btn-del")) {
-          this.delOne();
-        }
-
-        //informado o resultado
-        if (el.classList.contains("btn-eq")) {
-          this.equals();
-        }
-      });
-    },
-
-    //imprimindo valor no display
-    btnParaDisplay(valor) {
-      this.display.value += valor;
-    },
+      this.display.value = String(conta);
+    } catch (e) {
+      alert("Conta inválida");
+      return;
+    }
   };
 }
-//salvando função em uma variavel/constante
-const calc = criaCalc();
-//retornando função
-calc.init();
+
+//iniciando novo processo em uma função construtora (usar New)
+const calculadora = new Calc();
+//iniciando calculadora
+calculadora.inicia();
+
+// Prática ao longo do código de utilização de Arrow Functions
